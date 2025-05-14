c429a629c029ec78db1ab8a662673108857c3f36
martaverfer <martaverfer8@gmail.com>
2025-05-14 18:11:05 +0200

    changes in app - background

--- app/util.py
+++ app/util.py
@@ -16,18 +16,21 @@ def set_background(image_file):
     Returns:
         None
     """
-    with open(image_file, "rb") as f:
-        img_data = f.read()
-    b64_encoded = base64.b64encode(img_data).decode()
-    style = f"""
-        <style>
-        .stApp {{
-            background-image: url(data:image/png;base64,{b64_encoded});
-            background-size: cover;
-        }}
-        </style>
-    """
-    st.markdown(style, unsafe_allow_html=True)
+    try:
+        with open(image_file, "rb") as f:
+            img_data = f.read()
+        b64_encoded = base64.b64encode(img_data).decode()
+        style = f"""
+            <style>
+            .stApp {{
+                background-image: url(data:image/png;base64,{b64_encoded});
+                background-size: cover;
+            }}
+            </style>
+        """
+        st.markdown(style, unsafe_allow_html=True)
+    except FileNotFoundError:
+        st.warning("⚠️ Background image not found. Using default background.")
 
 
 def classify(image, model):
